ff278fb553c6c0c03cb206363d9aa1eef4678a56
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2024-05-13 13:44:53 +0200

    Python 1,2 done

---
+++ Python/megoldás/Számok játéka/szamokjateka.py
@@ -0,0 +1,16 @@
+from random import randint
+probalkozasok = 0
+generaltszam = int(randint(1, 25))
+print('Szám legenerálva.')
+
+while probalkozasok <= 5:
+    tipp = int(input('\nÍrja be a tippét: '))
+    probalkozasok += 1
+    print(f'Még {5 - probalkozasok} próbálkozása maradt.')
+    if tipp == generaltszam:
+        probalkozasok += 5
+        input('Sikeresen kitalálta a számot.')
+    elif tipp > generaltszam:
+        print('Próbáljon egy kisebb számot.')
+    else:
+        print('Próbáljon egy nagyobb számot.')
